03b47b85b7dc26c41ba83c176b5984f7bd76c4a4
Drew Sartorius <duckydesign@gmail.com>
2016-03-16 08:03:24 -0400

    Edit Mode added

--- JS_Babel_React.js
+++ JS_Babel_React.js
@@ -1,14 +1,40 @@
 var Box = React.createClass({
+  editMode: false,
+  addBtnClick: function(){
+    this.editMode = true;
+    this.forceUpdate();
+  },
+  editBtnClick: function(){
+    this.editMode = true;
+    this.forceUpdate();
+  },
+  saveBtnClick: function(){
+    this.editMode = false;
+    this.forceUpdate();
+  },
   render: function(){
+    if(this.editMode){
+      return (
+        <div className="box">
+          <div className="left-side">
+            <List />
+          </div>
+          <div className="right-side">
+            <EditCard />
+            <Save saveClicked={this.saveBtnClick} />
+          </div>
+        </div>
+      );
+    }
     return (
       <div className="box">
         <div className="left-side">
           <List />
-          <Add />
         </div>
         <div className="right-side">
           <Card />
-          <Edit />
+          <Add addClicked={this.addBtnClick} />
+          <Edit editClicked={this.editBtnClick} />
           <Delete />
         </div>
       </div>
@@ -40,6 +66,21 @@ var Card = React.createClass({
   }
 });
 
+var EditCard = React.createClass({
+  render: function(){
+    return (
+      <div className="edit-card">
+        <label>Title</label>
+        <input type="text" name="title-txt" className="title" />
+        <label>Ingredients</label>
+        <input type="text" className="ingr-txt" />
+        <label>Directions</label>
+        <input type="text" className="dir-txt" />
+      </div>
+    );
+  }
+});
+
 var Ingredients = React.createClass({
   render: function(){
     return (
@@ -62,17 +103,23 @@ var Directions = React.createClass({
 });
 
 var Add = React.createClass({
+  clicked: function(){
+    this.props.addClicked();
+  },
   render: function(){
     return (
-      <button className="add-btn">Add</button>
+      <button onClick={this.clicked} className="add-btn">Add</button>
     );
   }
 });
 
 var Edit = React.createClass({
+  clicked: function(){
+    this.props.editClicked();
+  },
   render: function(){
     return (
-      <button className="edit-btn">Edit</button>
+      <button className="edit-btn" onClick={this.clicked}>Edit</button>
     );
   }
 });
@@ -81,6 +128,17 @@ var Delete = React.createClass({
   render: function(){
     return (
       <button className="del-btn">Delete</button>
+    );
+  }
+});
+
+var Save = React.createClass({
+  clicked: function(){
+    this.props.saveClicked();
+  },
+  render: function(){
+    return (
+      <button className="save-btn" onClick={this.clicked}>Save</button>
     );
   }
 });
